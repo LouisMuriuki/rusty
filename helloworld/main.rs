e953bbd74872e5dde4f96e9a0e3d0af16a6545fc
LouisMuriuki <luihugo247@gmail.com>
2024-02-19 23:29:46 +0300

    ownership and borrowing

--- helloworld/main.rs
+++ helloworld/main.rs
@@ -1,3 +1,21 @@
+use std::io;
 fn main() {
-    println!("Hello World")
+    loop {
+        println!("Hello World");
+
+        println!("Please guess a number");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess).expect("error")
+            
+        let guess=match guess
+            .trim()
+            .parse()
+            {
+                Ok(num)=>num,
+                Err(_)=>continue,
+        };
+    }
 }
