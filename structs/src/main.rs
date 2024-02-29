8c9baedc030583adfb9bae86e4dbf3d73677702f
luihugo <luihugo247@gmail.com>
2024-02-29 22:07:06 +0300

    using methods with structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -4,11 +4,16 @@ struct User {
     active: bool,
     sign_in_count: u64,
 }
+#[derive(Debug)] //we must add this in-order to log the struct
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
 struct Color(i32, i32, i32); // tuple_struct
 fn main() {
     let black = Color(0, 0, 0);
-    
+
     let mut user1 = User {
         active: true,
         name: String::from("someusername123"),
@@ -38,9 +43,58 @@ fn main() {
     };
 
     println!("{}", user1.name); //user already moved, hence it is inavalid
+    println!("The area is {}", cal_area(4, 4));
+
+    let dimensions = (4, 4); // tuples dont name their elements, which is height and width???
+    println!("The area using tuples is {}", calc_area_tuple(dimensions));
+
+    let rectangle = Rectangle {
+        width: 4,
+        height: 4,
+    };
+
+    println!(
+        "The area using struct methods is {}",
+        calc_area_structs(&rectangle)
+    ); // we want main to retain ownership (ideally) hence the reference
+
+    println!(
+        "we can't direclty log a struct since it does not implemnt the Display trait {:#?}",
+        rectangle
+    ); //{:?} also works but for relatively smaller structs
+
+    println!(
+        "we can't direclty log a struct since it does not implemnt the Display trait we use this instead {:#?}",
+        rectangle
+    ); //{:?} also works but for relatively smaller structs
+
+    dbg!(&rectangle); // works as println! but takes ownership, refer to notes
+
+    // passing methods to struct instance
+    println!(
+        "The area using struct methods is {}",
+        rectangle.calc_area_structs_methods() //note calling methods, has automatic referncing and deferencing, meaning we dont have to pass refernce to rectangle
+    );
+
+    // Methods with more parameters -by default methods have a default parameter &self
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
-// we can also create return an instance of a struct from a function
+// we can also return an instance of a struct from a function
 // fn build_user(email:String, name:String) -> User {
 //     User {
 //         email,
@@ -49,3 +103,33 @@ fn main() {
 //         sign_in_count: 1,
 //     }
 // }
+
+// Area of a rectangle without structsb just ungrouped variables even though they are related
+
+fn cal_area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+// we could use structs to group them, lets see:
+fn calc_area_tuple(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1 // tuples dont name their elements hence we have to deal with this shit!!! structs???
+}
+
+// with structs
+fn calc_area_structs(rect: &Rectangle) -> u32 {
+    rect.height * rect.width
+}
+
+//Defining methods in structs
+impl Rectangle {
+    // everything with this implementation will be associated with rectangle type
+    fn calc_area_structs_methods(&self) -> u32 {
+        //self here is an alias of the type the impl block is for
+        self.width * self.height
+    }
+}
+impl Rectangle {
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.width * self.height > rect.height * rect.width
+    }
+}
