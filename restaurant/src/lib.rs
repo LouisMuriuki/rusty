8b8ff9de86baf74d8044af0f376e438f4819b6fc
luihugo <luihugo247@gmail.com>
2024-03-07 08:55:23 +0300

    library crate

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+mod front_of_house {
+    //curly brackets body of the module
+    mod hosting { //we can place other modules inside a module
+    //    can hold defination of other items e.g structs,enums, constants, traits
+        fn add_to_waitlist() {}
+        fn seen_at_table() {}
+    }
+//servings and hosting are siiblings
+    mod serving {//we can place other modules inside a module
+        
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
