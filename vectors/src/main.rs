09825f2f5bb026e04ca391d6c2d2e2569bd26abb
luihugo <luihugo247@gmail.com>
2024-03-12 11:23:47 +0300

    understanding vectors

---
+++ vectors/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let mut vec1=vec![1,2,3,4]; // using macros
+    let mut vec2:Vec<i32>=Vec::new(); //creates a new empty vector, you must explicitly type here
+    vec1.push(4);
+    vec2.push(4);
+}
