23a640b95f30a1a004fea3b3f9094d4b7fb3773d
luihugo <luihugo247@gmail.com>
2024-04-13 16:28:47 +0300

    Recoverable errors with panic

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::{fs::File, io::ErrorKind};
 fn main() {
     // panic!("crash and motherfucking burn");
     could_fail()
@@ -7,8 +7,24 @@ fn main() {
 // Handling Errors with result
 fn could_fail() {
     let greeting_file_result = File::open("hello.txt");
-    let greeting_file= match greeting_file_result {
-        Ok(file)=>file,
-        Err(error)=>panic!("The program panicked because{:?}",error),
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,  //Result enum is included in prelude just like Option enum hence we dont load it as Result::Ok
+        Err(error) => panic!("The program panicked because{:?}", error),
+    };
+}
+
+fn could_fail_handle_multiple() {
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {  //ErrorKind enum not included in prelude hence we load it here
+                Ok(fc) => fc,
+                Err(e) => panic!("problem creatting file{:?}", e),
+            },
+            other_error => {
+                panic!("problem creatting file{:?}", other_error)
+            }
+        },
     };
 }
