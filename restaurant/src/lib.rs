ccd31bfd1e31c29f162a2725de9ebf02fe3f1558
luihugo <luihugo247@gmail.com>
2024-06-05 10:05:03 +0300

    modules and collections recap

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -15,14 +15,15 @@ mod tests {
 
 mod front_of_house {
     //curly brackets body of the module
-    mod hosting { //we can place other modules inside a module
-    //    can hold defination of other items e.g structs,enums, constants, traits
+    mod hosting {
+        //we can place other modules inside a module
+        //    can hold defination of other items e.g structs,enums, constants, traits
         fn add_to_waitlist() {}
         fn seen_at_table() {}
     }
-//servings and hosting are siiblings
-    mod serving {//we can place other modules inside a module
-        
+    //servings and hosting are siblings
+    mod serving {
+
         fn take_order() {}
         fn serve_order() {}
         fn take_payment() {}
