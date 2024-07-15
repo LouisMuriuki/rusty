d5856ad7f600a6a820b6392c72decadb05e835c1
luihugo <luihugo247@gmail.com>
2024-07-15 08:27:16 +0300

    improved error handling-minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,14 +3,14 @@ use minigrep::{Config,run};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        print!("{}", err);
+        eprint!("Problem parsing the arguments{}", err);
         process::exit(2);
     });
 
     println!("The query is {:?}", config.query);
     println!("The filepath is {:?}", config.file_path);
     if let Err(e) = run(config) {
-        println!("{:?}", e);
+        eprintln!("Application Errror{:?}", e);
         process::exit(1);
     }
     // run(config).unwrap_or_else(|e|{
