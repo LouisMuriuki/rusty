2eb11c493be196c31dcaa6bd414ea3b4d77b6ceb
luihugo <luihugo247@gmail.com>
2024-07-20 11:22:15 +0300

    Iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -20,16 +20,24 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+// pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+//     let mut vector = vec![];
+//     for line in contents.lines() {
+//         if line.contains(query) {
+//             vector.push(line)
+//         }
+//     }
+//     vector
+//     // vec![]
+// }
+
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut vector = vec![];
-    for line in contents.lines() {
-        if line.contains(query) {
-            vector.push(line)
-        }
-    }
-    vector
-    // vec![]
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
+
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut vector = vec![];
     let query_search = query.to_lowercase();
@@ -41,14 +49,36 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
     }
     vector
 }
+// impl Config {
+//     pub fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+//         println!("{:#?}", args);
+//         if args.len() < 3 {
+//             return Err("please make sure to pass enough arguments");
+//         }
+//         let query = args[1].clone();
+//         let file_path = args[2].clone();
+//         let ignore_case = env::var("IGNORE_CASE").is_ok();
+//         Ok(Config {
+//             query,
+//             file_path,
+//             ignore_case,
+//         })
+//     }
+// }
+
 impl Config {
-    pub fn build(args: &Vec<String>) -> Result<Config, &'static str> {
-        println!("{:#?}", args);
-        if args.len() < 3 {
-            return Err("please make sure to pass enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build( mut args: impl Iterator<Item = String>,) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
         Ok(Config {
             query,
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,8 +1,8 @@
 use std::{env, process};
 use minigrep::{Config,run};
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    // let args: Vec<String> = env::args().collect();
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprint!("Problem parsing the arguments{}", err);
         process::exit(2);
     });
