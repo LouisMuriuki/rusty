3bea15273efe53a2387657a3f1d86a94dd6bd6be
luihugo <luihugo247@gmail.com>
2024-07-21 15:11:07 +0300

    more about cargo and crates.io

---
+++ cargo_more/src/main.rs
@@ -0,0 +1,22 @@
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+fn main() {
+    add_one(7);
+    // println!("Hello, world!");
+}
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
