7f6e93c57d3752328ea7599d1c7ced5f1c68165a
luihugo <luihugo247@gmail.com>
2024-07-31 09:45:25 +0300

    Rc smartPointer

--- smartpointers/src/main.rs
+++ smartpointers/src/main.rs
@@ -1,5 +1,25 @@
-use::dropPointer;
+use std::rc::Rc;
+use crate::List::{Nil,Cons};
+enum List{
+    Cons(i32, Rc<List>),
+    Nil
+}
 fn main() {
     let b = Box::new(5);
     println!("b ={b}");
+    main2();
+}
+
+fn main2() {
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+
+    //note there is also a weak_count which we will see later on.
 }
